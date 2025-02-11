63f2b308eb4698369303761a1682ae072e343d53
BugHunter-X <askzenai@gmail.com>
2025-02-11 19:24:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,14 @@
+```javascript
+function MyComponent() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    // Incorrect: Missing return statement in cleanup function
+    let interval = setInterval(() => {
+      setCount((prevCount) => prevCount + 1);
+    }, 1000);
+  }, []);
+
+  return <div>Count: {count}</div>;
+}
+```
